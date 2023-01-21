232626702425fcde75522dee2e966508cda7fa45
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-20 23:36:07 -0300

    chore: init spring boot project

---
+++ src/main/java/com/example/crudlover/CrudLoverApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudlover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudLoverApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/crudlover/controllers/Account.java
@@ -0,0 +1,4 @@
+package com.example.crudlover.controllers;
+
+public class Account {
+}
